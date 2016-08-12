210f79a4f7a520315d389b0a91d95d60824323a0
lgants <lgants@gmail.com>
2016-08-12 17:44:51 -0400

    pirates lab complete -ltg

--- app/models/pirate.rb
+++ app/models/pirate.rb
@@ -1,2 +1,20 @@
 class Pirate
+  attr_accessor :name, :weight, :height
+
+  def self.all
+    @@pirates
+  end
+
+  @@pirates = []
+
+  def self.clear
+    @@pirates = []
+  end
+
+  def initialize(options)
+    @name = options[:name]
+    @weight = options[:weight]
+    @height = options[:height]
+    @@pirates << self
+  end
 end
--- app/models/ship.rb
+++ app/models/ship.rb
@@ -1,2 +1,20 @@
 class Ship
+  attr_accessor :name, :type, :booty
+
+  def self.all
+    @@ships
+  end
+
+  @@ships = []
+
+  def self.clear
+    @@ships = []
+  end
+
+  def initialize(options)
+    @name = options[:name]
+    @type = options[:type]
+    @booty = options[:booty]
+    @@ships << self
+  end
 end
